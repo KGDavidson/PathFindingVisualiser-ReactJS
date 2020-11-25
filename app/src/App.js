783704b60fba7a683a8baa48831455f8f75bd8ce
K Davidson <kreatord@gmail.com>
2020-11-25 22:46:36 +0000

    Added Dummy NavBar

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import PathFindingVisualiser from './PathFindingVisualiser/PathFindingVisualiser'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="navBar">
+      </div>
+      <PathFindingVisualiser/>
+    </div>
+  );
+}
+
+export default App;
